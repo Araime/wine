7d205d4c1fa17b581504879e289ab6f7c703f243
Araime <alvonsa09121985@gmail.com>
2021-05-10 19:33:05 +0400

    Add new functional in main.py, new imports

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import datetime
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+today = datetime.datetime.now()
+current_year = today.year
+
+
+def get_age(current_year):
+    foundation_date = 1920
+    past_years = current_year - foundation_date
+    return f'Уже {past_years} год с вами'
+
+
+winery_age = get_age(current_year)
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    winery_age=winery_age
+)
+
+with open('index.html', 'w', encoding='utf8') as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
