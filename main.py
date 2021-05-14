06143de3077f55659a3ffbee4e9ea111a3806450
Araime <alvonsa09121985@gmail.com>
2021-05-14 20:28:34 +0400

    Change variables names

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 def get_age(current_year):
     foundation_date = 1920
-    lifetime = current_year - foundation_date
-    return f'Уже {lifetime} год с вами'
+    how_old_is_the_winery = current_year - foundation_date
+    return f'Уже {how_old_is_the_winery} год с вами'
 
 
 if __name__ == '__main__':
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     parser.add_argument('file_path', help='Необходимо в качестве аргумента при запуске указать'
                                           ' полный путь к файлу')
     args = parser.parse_args()
-    excel_file = args.file_path
+    prepared_table_with_wines = args.file_path
 
     today = datetime.datetime.now()
     current_year = today.year
@@ -30,25 +30,25 @@ if __name__ == '__main__':
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    wines_from_excel = pandas.read_excel(
-        excel_file,
+    derived_from_excel_wines = pandas.read_excel(
+        prepared_table_with_wines,
         sheet_name='Лист1',
         na_values=' ',
         keep_default_na=False
     )
 
-    wines_converted_to_dictionary = wines_from_excel.to_dict(orient='record')
+    wines_converted_to_dictionary = derived_from_excel_wines.to_dict(orient='record')
     grouped_wines = collections.defaultdict(list)
     for wine in wines_converted_to_dictionary:
         key = wine.get('Категория')
         grouped_wines[key].append(wine)
 
-    sorted_by_order_of_wine = OrderedDict(sorted(grouped_wines.items()))
+    ordered_wines = OrderedDict(sorted(grouped_wines.items()))
 
     template = env.get_template('template.html')
     rendered_page = template.render(
         winery_age=winery_age,
-        sorted_by_order_of_wine=sorted_by_order_of_wine
+        ordered_wines=ordered_wines
     )
 
     with open('index.html', 'w', encoding='utf8') as file:
