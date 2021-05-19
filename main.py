b1691e2f1240d2ebb44a90860a837a74b2e9c015
Araime <alvonsa09121985@gmail.com>
2021-05-19 20:44:08 +0400

    Add correct display of years by words

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 
 def get_age(current_year):
-    foundation_date = 1920
-    age_of_the_winery = current_year - foundation_date
-    return f'Уже {age_of_the_winery} год с вами'
+    foundation_year = 1925
+    age_of_the_winery = current_year - foundation_year
+    return age_of_the_winery
+
+
+def get_correct_word(age):
+    if age % 10 == 1 and age != 11 and age % 100 != 11:
+        word = 'год'
+    elif 1 < age % 10 <= 4 and age != 12 and age != 13 and age != 14:
+        word = 'года'
+    else:
+        word = 'лет'
+    return word
 
 
 if __name__ == '__main__':
@@ -19,25 +29,27 @@ if __name__ == '__main__':
     parser.add_argument('file_path', help='Необходимо в качестве аргумента при запуске указать'
                                           ' полный путь к файлу')
     args = parser.parse_args()
-    prepared_table_with_wines = args.file_path
+    path_to_the_table = args.file_path
 
     today = datetime.datetime.now()
     current_year = today.year
-    winery_age = get_age(current_year)
+    age = get_age(current_year)
+    correct_word = get_correct_word(age)
+    winery_age = f'Уже {age} {correct_word} с вами'
 
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    derived_from_excel_wines = pandas.read_excel(
-        prepared_table_with_wines,
+    wines_from_table = pandas.read_excel(
+        path_to_the_table,
         sheet_name='Лист1',
         na_values=' ',
         keep_default_na=False
     )
 
-    wines_converted_to_dictionary = derived_from_excel_wines.to_dict(orient='record')
+    wines_converted_to_dictionary = wines_from_table.to_dict(orient='records')
     grouped_wines = collections.defaultdict(list)
     for wine in wines_converted_to_dictionary:
         key = wine.get('Категория')
